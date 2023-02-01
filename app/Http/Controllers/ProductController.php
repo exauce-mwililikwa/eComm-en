fe96cd3ab16b6da7f0c0f4f7b19c2c34df311796
exauce mwililikwa <78547989+exauce-mwililikwa@users.noreply.github.com>
2023-02-01 11:53:15 +0200

    join cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use Session;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     function index(){
@@ -37,5 +38,11 @@ class ProductController extends Controller
 static function cartItem(){
     $userId=isset(Session::get('user')['id']);
     return Cart::where('user_id', $userId)->count();
+}
+function cartList(){
+    $products=DB::table('cart')
+    ->join('products','cart.product_id','=','products.id')
+    ->where('cart.user_id', Session::get('user')['id'])->get();
+    return view('cartList',['products'=>$products]);
 }
 }
